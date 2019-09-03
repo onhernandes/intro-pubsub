a82cfa612dd64714f7551f07d878cb5861363fc2
Matheus Hernandes <midia.matheus@gmail.com>
2019-09-03 18:21:13 -0300

    add pubsub script

---
+++ sendToPubsub.js
@@ -0,0 +1,24 @@
+const { PubSub } = require('@google-cloud/pubsub');
+const pubsub = new PubSub();
+
+const users = [
+  {
+    name: 'Seu Nome',
+    email: 'seu@email.com',
+    quantidadeVendas: 150
+  }
+];
+
+(async () => {
+  const topic = 'sendEmailToUserTopic';
+  const publisher = pubsub.topic(topic).publisher();
+  const promises = users
+    .map(user => {
+      const jsonUser = JSON.stringify(user);
+      const bufferedData = Buffer.from(jsonUser);
+      return publisher.publish(bufferedData);
+    });
+
+  await Promise.all(promises);
+  console.log(`Dados enviados para o tópico: ${topic}!`);
+})();
