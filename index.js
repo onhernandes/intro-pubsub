f1feb06e9ed546b23b6a60cb8b68c279ed704853
Matheus Hernandes <midia.matheus@gmail.com>
2019-09-03 14:48:27 -0300

    code send function

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-exports.sendEmailToUser = (event, cb) => {
-  response.status(200).send('Hello World!');
+const SparkPost = require('sparkpost');
+const spark = new SparkPost('key');
+
+const getEventData = (event) => {
+  const eventData = _.get(event, 'data');
+
+  if (eventData === undefined) {
+    return {};
+  }
+
+  let message = eventData.data ? Buffer.from(eventData.data, 'base64').toString() : eventData;
+
+  if (!message) {
+    return {};
+  }
+
+  if (typeof message === 'string') {
+    const originalMessage = message;
+
+    try {
+      message = JSON.parse(message);
+    } catch (e) {
+      message = originalMessage;
+    }
+  }
+
+  return message;
+}
+
+exports.sendEmailToUser = async (event) => {
+  const data = getEventData(event);
+  let payload = {
+    content: {
+      subject: 'Relatório de vendas',
+      from: 'sua@empresa.com.br'
+    },
+    recipients: [ { address: data.email } ],
+    html: `Ola ${data.nome}, essa semana tivemos um total de ${data.quantidadeVendas} vendas!`
+  };
+
+  const response = await spark.transmissions.send(payload);
+
+  console.log('Email enviado!', response);
+  return response;
 };
